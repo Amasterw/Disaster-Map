188672433c0a2449e94d02320b7dcd2033763f5f
Amasterw <agrwash@gmail.com>
2021-01-03 19:39:09 -0500

    update 7

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,39 +1,29 @@
-function createMaps(Earthquake){
-    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+function createMap(Earthquake){
+    var myMap = L.map("map-id", {
+        center: [37.09, -95.71],
+        zoom: 4,
+        layer: [stmap, Earthquake]
+    });
+
+    var stmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
-        id: "light-v11",
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
         accessToken: API_KEY
-    });
-
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        id: "dark-v11",
-        accessToken: API_KEY
-    });
-
-    var baseMaps ={"Dark Map": darkmap,
-        "Light Map": lightmap} 
-    var overlayMaps = {"Earthquake Map": Earthquake}
-
-    var myMap = L.map("mapid", {
-        center: [37.090, -95.712],
-        zoom: 10,
-        layer: [lightmap, darkmap, Earthquake]
-    });
-
-    L.control.layers(baseMaps, overlayMaps, {
-        collaspsed: false
     }).addTo(myMap);
 
-    
-}
+    var baseMaps ={"Street Map": stmap};
+  
+    var overlayMaps = {"Earthquake Map": Earthquake};
+
+    L.control.layers(baseMaps, overlayMaps, {
+    collaspsed: false
+    }).addTo(myMap);
+};
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson", createMarkers);
-
 
 function createMarkers(earthquakeData) {
     console.log(earthquakeData);
@@ -47,5 +37,5 @@ function createMarkers(earthquakeData) {
         "</h3><hr><p>Magnitude: " + feature.properties.mag + "</p>"); 
         earthquakeMarkers.push(earthquakeMarker);
     }
-    createMaps(L.layerGroup(earthquakeMarkers));
+    createMap(L.layerGroup(earthquakeMarkers));
  };
